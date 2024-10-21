6ee78c5979c38973021cdfbe43c9372d3a93a46a
camfung <cameronfuung@gmail.com>
2024-10-21 15:30:54 -0700

    added not null to user

--- backend/models/user.go
+++ backend/models/user.go
@@ -4,13 +4,13 @@ import "time"
 
 // User represents the user of the system
 type User struct {
-	ID                 uint   `gorm:"primaryKey"`
-	Username           string `gorm:"unique"`
-	FirstName          string
-	LastName           string
-	Email              string
-	Password           string
-	Configs            []UserConfig   `gorm:"foreignKey:UserID"`
-	DeletedAt          *time.Time     `gorm:"index"`
-	CreatedAt          *time.Time     `gorm:"index"`
+	ID        uint         `gorm:"primaryKey"`
+	Username  string       `gorm:"unique"`
+	FirstName string       `gorm:"not null: True"`
+	LastName  string       `gorm:"not null: True"`
+	Email     string       `gorm:"not null: True"`
+	Password  string       `gorm:"not null: True"`
+	Configs   []UserConfig `gorm:"foreignKey:UserID"`
+	DeletedAt *time.Time   `gorm:"index"`
+	CreatedAt *time.Time   `gorm:"index"`
 }
