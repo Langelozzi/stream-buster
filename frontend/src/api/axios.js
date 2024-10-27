f7b030136706469cada57e471fcb128cf258ae6c
camfung <cameronfuung@gmail.com>
2024-10-27 12:29:09 -0700

    added axios instance

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// Create an instance of axios and set the default configurations
+const instance = axios.create({
+	baseURL: import.meta.env.VITE_SERVER_URL,
+	withCredentials: true
+});
+
+export default instance;
