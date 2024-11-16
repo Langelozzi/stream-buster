63ec29d8d3b5a8f6e8b5a72ef83c874029cedf54
camfung <cameronfuung@gmail.com>
2024-11-16 14:53:29 -0800

    updated api services in front end

--- frontend/src/api/services/currentlyWatching.service.ts
+++ frontend/src/api/services/currentlyWatching.service.ts
@@ -1,10 +1,17 @@
+import { CurrentlyWatching } from "../../models/currently_watching";
 import axios from "../axios";
 
 export const getCurrentlyWatching = async () => {
     const result = await axios.get("/currently-watching/getall");
     console.log('result', result);
 }
-export const getWatchList = async () => {
+
+export const createCurrentlyWatching = async (data: any) => {
+    const result = await axios.post("/currently-watching/", data)
+    return result
+}
+
+export const getWatchList = async (): Promise<CurrentlyWatching[]> => {
     const result = await axios.get("/currently-watching/watchlist");
-    console.log('result', result);
+    return result.data;
 }
--- frontend/src/api/services/media.service.ts
+++ frontend/src/api/services/media.service.ts
@@ -1,3 +1,4 @@
+import { Media } from "../../models/media";
 import instance from "../axios";
 
 export const getMedia = (id: number) => {
@@ -7,5 +8,15 @@ export const getMedia = (id: number) => {
         return res
     } catch (error) {
         return error
+    }
+}
+
+export const createMedia = async (media: Media) => {
+    try {
+        const res = instance.post("/media/create", media);
+        console.log('res', res);
+        return res
+    } catch (error) {
+        throw error
     }
 }
